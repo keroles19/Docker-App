dee44f3512199267449b1d5dcaf607b11d5a7eb4
keroles19 <kerolesatef200@gmail.com>
2023-03-20 23:10:23 +0200

    pass variable from docker-compose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 // init app 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get('/',(req, res) => res.send('<h1> Hello Wolrd </h1>'));
