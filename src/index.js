9dbf176e0aa703849930f94f01fc011117ffbb80
keroles19 <kerolesatef200@gmail.com>
2023-06-18 23:31:11 +0300

    mult stage environment : section 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const express = require('express')
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.get('/',(req, res) => res.send('<h1> Hello Wolrd </h1>'));
+app.get('/',(req, res) => res.send('<h1> Hello Wolrd</h1>'));
 
 app.listen(PORT, () => console.log(`app is running on port: ${PORT}`));
