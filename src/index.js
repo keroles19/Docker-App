d1c3950b1b0480d7c2327d5b4d7c150f5059e79b
keroles19 <kerolesatef200@gmail.com>
2023-06-23 02:04:55 +0300

    postgresql with docker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const redis    = require('redis');
+// const {Client} = require('pg');
 
 // init app 
 const PORT = process.env.PORT || 4000;
@@ -17,15 +18,6 @@ redisClient.on('error', err => console.log('Redis Client Error', err));
 redisClient.on('connect', () => console.log('Redis Connected ...'));
 redisClient.connect();
 
-// connect to db
-const DB_USERNAME = 'root';
-const DB_PASSWORD = 'example'
-const DB_PORT = 27017;
-const DB_HOST = 'mongo';
-const URI = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-mongoose
-    .connect(URI).then(() => console.log('connected to db ... ')).catch((err) => console.log('err', err));
-
 app.get('/', (req, res) => {
     redisClient.set('producs', 'products Lists....');
     res.send('<h1> Hello Wolrd</h1>')
@@ -35,5 +27,36 @@ app.get('/producs', async (req, res) => {
     const products = await redisClient.get('producs');
     res.send(`<h1> Hello Wolrd</h1> <h2>${products}</h2>`)
 });
+
+// End Redis
+
+
+//MONGO
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'example'
+const DB_PORT = 27017;
+const DB_HOST = 'mongo';
+const URI = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+    .connect(URI).then(() => console.log('connected to db ... ')).catch((err) => console.log('err', err));
+
+// End Mongo
+
+
+// Postgress
+
+// const DB_USERNAME = 'root';
+// const DB_PASSWORD = 'example'
+// const DB_PORT = 5432;
+// const DB_HOST = 'postgres';
+
+// const URI = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+// const client = new Client({   connectionString: URI});
+// client.connect()
+//   .then(() => console.log('connected to db ... '))
+//   .catch((err) => console.log('Error connecting to PostgreSQL:', err));
+
+// End Postgress
+
 
 app.listen(PORT, () => console.log(`app is running on port: ${PORT}`));
